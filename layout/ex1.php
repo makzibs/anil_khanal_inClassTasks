4b1dfcc982dc6782214896974b040e216b9020ee
Anil Khanal <66034315+makzibs@users.noreply.github.com>
2023-02-03 00:43:13 +0545

    Finally done

--- layout/ex1.php
+++ layout/ex1.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
+
+<?php include "header.php" ?>
 
 <h2>3.1 Write a simple PHP script to print your information (Name and your groupid). </h1>
 
@@ -72,6 +66,7 @@ echo "
 <h2>4 Screenshot of the development of environment</h2>
 <img src="./screenshot.jpg" alt="screenshot of dev env">
 
-</body>
-</html>
+<?php include "footer.php" ?>
+
+
 
