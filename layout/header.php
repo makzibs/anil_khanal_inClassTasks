d2ac0760069fdaece3ae135b59c59b7ebb9dbdce
Anil Khanal <66034315+makzibs@users.noreply.github.com>
2023-02-03 00:26:47 +0545

    Universal header and footer

--- layout/header.php
+++ layout/header.php
@@ -6,14 +6,127 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
     <title>Catffee</title>
-    <link rel="stylesheet" href="style.css">
+    <!-- <link rel="stylesheet" href="style.css"> -->
+    <style>
+        body{
+    font-family: 'Poppins', sans-serif;
+    margin: 0;
+    padding: 0;
+   }
+   
+   nav {
+   
+       background-color: #A4D8CC;
+       display: flex;
+       flex-wrap: wrap;
+       height: 123px;
+       justify-content: space-around;
+   }
+   
+   .logoText{
+       margin-left: 30px;
+       display: flex;
+       justify-content: flex-start;
+       align-items: center;
+       flex-grow: 1;
+   
+   }
+   
+    .navItem1{
+        width: 60px;
+        height: 60px;
+    }
+
+   .navItem1 img{
+        max-width: 100%;
+        max-height: 100%;
+   }
+   .navItem2{
+       margin-left: 30px;
+       font-size: 38px;
+       color: white;
+   }
+   
+   .navRest{
+       margin-right: 30px;
+       display: flex;
+       justify-content: flex-end;
+       align-items: center;
+       flex-grow: 1;
+   }
+   .navItem3{
+       font-size: 38px;
+       font-weight: bold;
+       color: #007777;
+       
+   }
+   .navItem3 a{
+    font-size: 32px;
+    color: white;
+   }
+   .navItem4{
+       font-size: 32px;
+       color: white;
+       margin-left: 30px;
+   }
+   .navItem4 a{
+    font-size: 32px;
+    color: white;
+    margin-left: 30px;
+   }
+   .navItem5{
+       font-size: 32px;
+       color: white;
+       margin-left: 30px;
+   }
+
+   .navItem5 a{
+    font-size: 32px;
+    color: white;
+    margin-left: 30px;
+   }
+   .navItem6{
+       font-size: 32px;
+       color: white;
+       margin-left: 30px;
+   }
+
+   .navItem6 a{
+    font-size: 32px;
+    margin-left: 30px;
+    color: white
+}
+/*footer css*/
+.footer{
+       background-color: #A4D8CC;
+       height: 123px;
+       color: white;
+       display: flex;
+       align-items: center;
+       
+   }
+   .footerInsta{
+       margin-left: 30px;
+       font-size: 38px;
+       
+   }
+   .footerFB{
+       font-size: 38px;
+       margin-left: 30px;
+   }
+   .footerTwitter{
+       font-size: 38px;
+       margin-left: 30px;
+   }
+
+    </style>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,700;1,100;1,600&family=Roboto:wght@100;300;500&display=swap" rel="stylesheet">
 
 </head>
 <body>
    <nav>
     <div class = "logoText">
-     <div class = "navItem1"><img src="./images/logo.png" alt="Logo"></div>
+     <div class = "navItem1"><img src="./logo.png" alt="Logo"></div>
      <div class = "navItem2">Catffee</div>
     </div>
     <div class = "navRest">
